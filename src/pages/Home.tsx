02971d81078f2a55aa378c182fd0a54bf0b6f610
Bruno Lima <francisco.bruno@armateus.com.br>
2022-02-14 19:11:00 -0300

    mudando para typescript

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,16 +11,31 @@ import {
 import { Button } from '../components/Button';
 import { SkillCard } from '../components/SkillCard';
 
+interface SkillData {
+  id: string;
+  name: string;
+}
+
 export function Home(){
   const [newSkill, setNewSkill] = useState('');
-  const [mySkills, setMySkills] = useState([]);
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
   const [grettings, setGreetings] = useState('');
 
-
   function handleAddNewSKill(){
-    setMySkills(oldState => [...oldState, newSkill]);
+
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill
+    }
+
+    setMySkills(oldState => [...oldState, data]);
   }
 
+  function handleRemoveSkill(id: string){
+    setMySkills(oldState => oldState.filter(
+      skill => skill.id !== id
+    ));
+  }
   useEffect(() => {
     const currentHour = new Date().getHours();
     if(currentHour < 12) {
@@ -45,7 +60,7 @@ export function Home(){
         onChangeText={setNewSkill}
       />
 
-      <Button onPress={handleAddNewSKill} />
+      <Button title="Add" onPress={handleAddNewSKill} />
 
       <Text style={[styles.title, { marginVertical: 50}]}>
         My Skills
@@ -53,9 +68,9 @@ export function Home(){
 
       <FlatList
         data={mySkills}
-        keyExtractor={item => item}
+        keyExtractor={item => item.id}
         renderItem={({ item}) => (
-          <SkillCard skill={item} />
+          <SkillCard skill={item.name} onPress={() => handleRemoveSkill(item.id)} />
         )}
       />
     </View>
@@ -66,9 +81,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#121015',
-    paddingHorizontal: 20,
-    paddingVertical: 70,
     paddingHorizontal: 30,
+    paddingVertical: 70,
   },
   title: {
     color: '#FFF',
